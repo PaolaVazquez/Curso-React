69a0179899c068c152ddfeeb2b4cae2d04cdd049
PaolaVazquez <paola.vazra98@gmail.com>
2023-02-23 11:55:45 -0600

    pasado

--- azami/src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-//rafc hace función con exportación de función
-//fafce hace función con exportación por default
-export const Footer = () => {
-  return (
-    <div>
-        <h1>Footer</h1>
-    </div>
-  )
-}
--- azami/src/Components/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-import './ItemListContainer.css'
-
-export const ItemListContainer = ({greeting}) => {
-  return (
-    <div>
-      <h1 className='titulo'>{greeting}</h1>
-    </div>
-  )
-}
