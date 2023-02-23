77faeff69e79ef45310f18ddab71d9d12cf39760
PaolaVazquez <paola.vazra98@gmail.com>
2023-02-23 11:57:28 -0600

    PreEntrega1VazquezRamos

---
+++ azami/src/App.js
@@ -0,0 +1,22 @@
+
+import { Footer } from "./Components/Footer/Footer";
+import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer";
+import { Navbar } from "./Components/Navbar/Navbar"; //importar por constante o función
+import { ProductCard } from "./Components/ProductCard/ProductCard";
+//import Navbar from "./Components/Navbar/Navbar"; //importar por defecto
+
+//{/* COMENTAR */}
+function App() {
+  return (
+    <div>
+      <Navbar/>
+  
+      <ItemListContainer greeting={"hola"}/>
+
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ azami/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import './ItemListContainer.css'
+
+export const ItemListContainer = ({greeting}) => {
+  return (
+    <div>
+      <h1 className='titulo'>{greeting}</h1>
+    </div>
+  )
+}
